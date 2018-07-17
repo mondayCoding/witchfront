0d54763dc0823befeda3a3aa9c761a7ce96432d5
mondayCoding <moriluxuria@gmail.com>
2018-07-18 00:28:57 +0300

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,11 @@ module.exports = {
 
    entry: {
       app: "./src/index.tsx",
-      vendor: ["react", "react-dom", "react-router", "react-select", "react-localization", "axios", "react-sortable-hoc", "mark.js", "toastr"],
-      serverApp: "./app.ts"
+      vendor: ["react", "react-dom", "react-router", "react-select", "react-localization", "axios", "react-sortable-hoc", "mark.js", "toastr"]
    },
 
    output: {
-      path: path.resolve(__dirname, "public/javascripts/"),
+      path: path.resolve(__dirname, "build/"),
       filename: "[name].js"
    },
 
@@ -48,39 +47,3 @@ module.exports = {
    ]
 };
 
-// const path = require("path");
-//
-// module.exports = {
-//    entry: "./src/index.tsx",
-//    output: {
-//       path: path.resolve(__dirname, "public/javascripts/"),
-//       filename: "bundle.js"
-//    },
-//    devtool: "source-map",
-//    resolve: {
-//       // Add '.ts' and '.tsx' as resolvable extensions.
-//       extensions: [".ts", ".tsx", ".js", ".json"]
-//    },
-//    module: {
-//       rules: [
-//          {
-//             test: /\.tsx?$/,
-//             loader: "awesome-typescript-loader"
-//          },
-//          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-//          {
-//             enforce: "pre",
-//             test: /\.js$/,
-//             loader: "source-map-loader"
-//          }
-//       ]
-//    },
-//    externals: {
-//       "react": "React",
-//       "react-dom": "ReactDOM",
-//       "jquery": 'jQuery'
-//    },
-      // new webpack.ProvidePlugin({
-      //    $: "jquery"
-      // })
-// };
