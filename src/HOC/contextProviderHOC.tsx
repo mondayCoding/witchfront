0dc997a875dcd23f54ea4863f166a3537c0cb867
nikaupp <40818855+nikaupp@users.noreply.github.com>
2018-08-09 11:36:54 +0300

    added hoc template

--- src/HOC/contextProviderHOC.tsx
+++ src/HOC/contextProviderHOC.tsx
@@ -13,3 +13,37 @@ const wrapInContext = (WrappedComponent:any) => {
 };
 
 export default wrapInContext;
+
+
+// *********************************************************************
+// HOC - TEMPLATES
+// *********************************************************************
+
+// Stateless High Order Component (HOC)
+const WrapWithStateless = (WrappedComponent:any) => {
+
+   // DO SOMETHING HERE
+
+   return (props:any) => {
+      return(
+         <WrappedComponent {...props} />
+      );
+   };
+};
+
+
+// Class High Order Component (HOC)
+const WrapWithClass = (WrappedComponent:any) => {
+   return class extends React.Component {
+
+      // DO SOMETHING HERE
+
+      render(){
+         return(
+            <WrappedComponent {...this.props} />
+         );
+      }
+   }; 
+};
+
+export { WrapWithClass, WrapWithStateless };
