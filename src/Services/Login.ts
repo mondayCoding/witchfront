1baa3feb2c6117ffae3aaca205a2e9235eea03e9
mondaycoding <moriluxuria@gmail.com>
2018-08-15 18:42:08 +0300

    accept

--- src/Services/Login.ts
+++ src/Services/Login.ts
@@ -9,18 +9,14 @@ export default class Login {
 
 	//get all items (promise)
 	public static async attemptLogin(params:any) {
-		return new Promise((resolve) =>
-			axios.post(service_url, params)
-				.then(
-					(response) => resolve(response.data)
-				)
-				.catch(               
-					(error) => { 
-                  resolve(error);
-                  LOGGING.LogErrorResponse(error, {announceErrors:false});
-               }
-				)
-		);
+
+      return axios.post(service_url, params)
+         .then(
+            (response) => response                      
+         )
+         .catch(               
+            (error:AxiosError) => error.response
+         );  
    }
    
 }
